7f045c21c6684eba524f147b95a1beef79155ee1
Sean <sean.leu@gmail.com>
2022-12-09 17:11:02 -0500

    Update

--- AoC-Day9-22-P2.py
+++ AoC-Day9-22-P2.py
@@ -1,114 +1,41 @@
-visited = [[0,0]]
-movement = [[0,0]]
-head = [0,0] #(x,y)
-prev_head = [0,0]
-moved = [0,0]
+knots = 2
+visited = []
+rope = [[0,0] for i in range(knots)]
 directions = []
 
-def move_right(count):
-    for c in range(int(count)):
-        head[0] +=1
-        if not(is_neck_in_range()):
-            track_movement2("R")
-        update_prev_head()
-
-def move_left(count):
-    for c in range(int(count)):
-        head[0] -=1
-        if not(is_neck_in_range()):
-            track_movement2("L")
-        update_prev_head()
-
-def move_up(count):
-    for c in range(int(count)):
-        head[1] +=1
-        if not(is_neck_in_range()):
-            track_movement2("U")
-        update_prev_head()
-
-def move_down(count):
-    for c in range(int(count)):
-        head[1] -=1
-        if not(is_neck_in_range()):
-            track_movement2("D")
-        update_prev_head()
-
-def is_neck_in_range():
-    if abs(head[0] - movement[0][0]) <= 1 and abs(head[1] - movement[0][1]) <= 1:
-        return True
-    else:
-        return False
-
-def track_movement():
-    move = [prev_head[0], prev_head[1]]
-
-    movement.insert(0, move)
-    if len(movement) == 10:
-        visits()
-
-def track_movement2(direction):
-    prev_position = [0,0]
-    for i in range(len(movement)):
-        if i == 0:
-            prev_position[0] = movement[0][0]
-            prev_position[1] = movement[0][1]
-            movement[0][0] = prev_head[0]
-            movement[0][1] = prev_head[1]
+def move(head, knot):
+    if abs(head[0] - knot[0]) <= 1 and abs(head[1] - knot[1]) <= 1:
+        return knot
+    for i in [0,1]:
+        if head[i]-knot[i] > 0:
+            knot[i] +=1
+        elif head[i]-knot[i] < 0:
+            knot[i] -=1
         else:
-            if direction == "L":
-                if movement[i][1] == prev_position[1]:
-                    movement[i][0] -=1
-                    movement[i][1] = movement[i-1][1]
-            elif direction == "R":
-                if movement[i][1] == prev_position[1]:
-                    movement[i][0] +=1
-                    movement[i][1] = movement[i-1][1]
-            elif direction == "U":
-                if movement[i][0] == prev_position[0]:
-                    movement[i][1] +=1
-                    movement[i][0] = movement[i-1][0]
-            elif direction == "D":
-                if movement[i][0] == prev_position[0]:
-                    movement[i][1] -=1
-                    movement[i][0] = movement[i-1][0]
-            else:
-                print("wtf")
-        #elif not(abs(movement[i-1][0] - movement[i][0]) <= 1 and abs(movement[i-1][1] - movement[i][1]) <= 1):
-        #       movement[i][0], movement[i][1] = prev_position[0], prev_position[1]
-        #print("Old: %s Prev %s Movement %s" % (old_position, prev_position, movement))
-    print(movement)
-    if len(movement) != 9: 
-        movement.append([0,0])
-    else:
-        visits()
+            pass
+    return knot
 
-def update_prev_head():
-    prev_head[0] = head[0]
-    prev_head[1] = head[1]
-
-def visits():
-    tail = [movement[-1][0], movement[-1][1]]
-    if not(tail in visited):
-        visited.append(tail)
-
-with open('sample.txt') as i:
+with open('input9.txt') as i:
     input = i.read().splitlines()
 
 for line in input:
     directions.append(line.split(' '))
 
 for step in directions:
-    match step[0]:
-        case "R":
-            print("R")
-            move_right(step[1])
-        case "L":
-            move_left(step[1])
-        case "U":
-            move_up(step[1])
-        case "D":
-            move_down(step[1])
-        case _:
-            print("Improper input with: %s" % step[0])
+    for i in range(int(step[1])):
+        match step[0]:
+            case "R":
+                rope[0][1] +=1
+            case "L":
+                rope[0][1] -=1
+            case "U":
+                rope[0][0] +=1
+            case "D":
+                rope[0][0] -=1
+        for k_index in range (1,knots):
+            rope[k_index] = move(rope[k_index-1], rope[k_index])
+        check_tail = [rope[-1][0], rope[-1][1]]
+        if not(check_tail in visited):
+            visited.append(check_tail)
 
 print("Total Visits: %d" % len(visited))
