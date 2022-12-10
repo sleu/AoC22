9e70a6e6bece42fea594f8ebaa5dbb5724b5b872
Sean <sean.leu@gmail.com>
2022-12-10 10:21:57 -0500

    Update

--- AoC-Day10-22.py
+++ AoC-Day10-22.py
@@ -1,11 +1,45 @@
-input_list = []
+WIDTH = 40
 
-with open('input9.txt') as i:
+input_list = []
+x = 1
+cycles = 0
+count = 0
+pause = 1
+output_cycles = [20, 60, 100, 140, 180, 220]
+output_cycles_values = []
+sprite_line = ['.' for i in range(WIDTH)]
+
+def draw(sprite_line=sprite_line):
+    cycle = (cycles % 40)-1
+    if cycle == -1:
+        cycle = 39
+    if cycle in range(x-1,x+2):
+        sprite_line[cycle] = '#'
+    if cycle == 39:
+        print(*sprite_line)
+        reset_line(sprite_line)
+
+def reset_line(sprite_line=sprite_line):
+    for x in range(len(sprite_line)):
+        sprite_line[x] = '.'
+
+with open('input10.txt') as i:
     input = i.read().splitlines()
 
 for line in input:
     input_list.append(line.split(' '))
 
-for step in input_list:
-    #TODO
-    break
+while count < len(input_list):
+    cycles +=1 #start of current cycle
+    draw(sprite_line)
+
+    step = input_list[count]
+    if step[0] == "noop":
+        count +=1
+    else: #addx
+        if pause < 2:
+            pause +=1
+        else:
+            x += int(step[1])
+            pause = 1
+            count +=1
