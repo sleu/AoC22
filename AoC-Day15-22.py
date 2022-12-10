b8dde0bcf4e504ff07f45d7ae9e5cffa01e15f52
Sean <sean.leu@gmail.com>
2022-12-09 21:23:40 -0500

    Update

---
+++ AoC-Day15-22.py
@@ -0,0 +1,11 @@
+input_list = []
+
+with open('input9.txt') as i:
+    input = i.read().splitlines()
+
+for line in input:
+    input_list.append(line.split(' '))
+
+for step in input_list:
+    #TODO
+    break
