f720ab6cb0e49bbe04714872a0c9d94ae5d4d1c5
sleu <sean.leu@gmail.com>
2022-12-15 23:45:15 -0500

    Update

--- AoC-Day15-22.py
+++ AoC-Day15-22.py
@@ -1,37 +1,5 @@
 input_list = []
-x_endpoints = [0,0]
-y_length = 0
-greatest_distance = 0
-map = []
 distances = []
-#[0] [0,1] sensor x,y :: [1] [0,1] beacon x,y
-
-def build_map():
-    width = abs(x_endpoints[0]) + abs(x_endpoints[1]) + 1 + 2*greatest_distance
-    length = y_length + 1 + 2*greatest_distance
-    for y in range(length):
-        map.append(["." for _ in range(width)])
-
-def covered():
-    for row in input_list:
-        s_x,s_y = offset(row[0][0]),offset(row[0][1])
-        b_x,b_y = offset(row[1][0]),offset(row[1][1])
-        d = abs(b_x - s_x) + abs(b_y - s_y) + 1
-        for x in range(d):
-            y_off = d-x
-            for y in range(y_off):
-                print(row,s_x,s_y)
-                #print(row,s_x + x-y, s_y + x-y,d)
-                map[s_y+y][s_x+x] = "#" 
-                map[s_y-y][s_x+x] = "#"
-                map[s_y+y][s_x-x] = "#"
-                map[s_y-y][s_x-x] = "#"
-        map[s_y][s_x] = "S"
-        map[b_y][b_x] = "B"
-
-def offset(value):
-    return value + greatest_distance
-
 
 def part_a(row):
     covered_count = []
@@ -47,8 +15,22 @@ def part_a(row):
             covered_count.extend(range(l, v[0][0]))
     return len(set(covered_count))
 
+def part_b():
+    covered_count = []
+    for i,v in enumerate(input_list):
+        up = v[0][1] - distances[i]
+        down = v[0][1] + distances[i]
+        left = v[0][0] - distances[i]
+        right = v[0][0] + distances[i]
+        if 0 in range(up,down):
+            #TODO
+            return 0
+        elif 4_000_000 in range(up,down):
+            return 0
 
-with open('inputs/sample.txt') as i: input = i.read().splitlines()
+
+
+with open('inputs/input15.txt') as i: input = i.read().splitlines()
 
 for line in input:
     l=line.split(" ")
@@ -65,28 +47,6 @@ for row in input_list:
         d = abs(b_x - s_x) + abs(b_y - s_y)
         distances.append(d)
 
-print(part_a(10))
+print(part_a(2000000))
 
-
-"""
-x_endpoints[0],x_endpoints[1] = input_list[0][0][0],input_list[0][0][1]
-for line in input_list:
-    x_min = min(line[0][0], line[1][0])
-    x_max = max(line[0][0], line[1][0])
-    y_max = max(line[0][1], line[1][1])
-    if x_endpoints[0] > x_min:
-        x_endpoints[0] = x_min
-    if x_endpoints[1] < x_max:
-        x_endpoints[1] = x_max
-    if y_length < y_max:
-        y_length = y_max
-    if greatest_distance < abs(line[0][0] - line[1][0]) + abs(line[0][1] - line[1][1]):
-        greatest_distance = abs(line[0][0] - line[1][0]) + abs(line[0][1] - line[1][1])
-greatest_distance +=1
-print(x_endpoints, y_length, greatest_distance)
-build_map()
-print("built")
-covered()
-#print_map()
-print(map[offset(10)].count("#"))
-"""
+#frequency = x*4000000 + y
--- playground.py
+++ playground.py
@@ -1,3 +1,3 @@
-x = [1,2,1,4]
+x = [1,2,3,4]
 x.extend((3,6))
-print(set(x))
+print(range(3,6) in range(0,40))
