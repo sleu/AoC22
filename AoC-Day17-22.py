0c42e1de64c67c14b5692d404f136bb3d11c0f7c
Sean <sean.leu@gmail.com>
2022-12-18 11:08:29 -0500

    Update

--- AoC-Day17-22.py
+++ AoC-Day17-22.py
@@ -1,5 +1,5 @@
-map = [['.' for _ in range(7)]] #y,x
-top_row = 3
+map = [] #y,x
+top_row = 0
 
 class Rock:
     def __init__(self, type): #position is bottom left coordinates
@@ -7,7 +7,7 @@ class Rock:
         self.position = [] #x,y
     def jet_move(self, dir):
         def check_right():
-            match type:
+            match self.type:
                 case 0: # -
                     if self.position[0]+3 == 6 or map[self.position[1]][self.position[0]+4] == "#":
                         return False #can't move
@@ -78,10 +78,11 @@ class Rock:
                         return True
         match dir:
             case ">":
-                if check_right(): self.position[0] +=1
+                if check_right(): self.position[0] +=1; #print("moved right")
             case "<":
-                if check_left(): self.position[0] -=1
+                if check_left(): self.position[0] -=1; #print("moved left")
     def down_move(self):
+        #print("in move down with y at %d" % self.position[1])
         match self.type:
             case 0: # -
                 if self.position[1] == 0:
@@ -92,7 +93,7 @@ class Rock:
                     map[self.position[1]-1][self.position[0]+3] == "#"):
                     return False #can't move
                 else:
-                    self.position[1] +=1
+                    self.position[1] -=1
                     return True
             case 1: # +
                 if self.position[1]-2 == 0:
@@ -100,7 +101,7 @@ class Rock:
                 elif map[self.position[1]-3][self.position[0]+1] == "#":
                     return False 
                 else:
-                    self.position[1] +=1
+                    self.position[1]-=1
                     return True
             case 2: # _|
                 if self.position[1]-2 == 0:
@@ -110,7 +111,7 @@ class Rock:
                     map[self.position[1]-3][self.position[0]+2] == "#"):
                     return False 
                 else:
-                    self.position[1] +=1
+                    self.position[1] -=1
                     return True
             case 3: # |
                 if self.position[1]-3 == 0:
@@ -127,10 +128,41 @@ class Rock:
                     map[self.position[1]-2][self.position[0]+1] == "#"):
                     return False
                 else:
-                    self.position[1] +=1
+                    self.position[1] -=1
                     return True
-    def imprint():
-        return 0 # TODO:list of coordinates
+    def imprint(self):
+        coordinates = []
+        match self.type:
+            case 0: # -
+                coordinates.append(self.position)
+                coordinates.append([self.position[0]+1,self.position[1]])
+                coordinates.append([self.position[0]+2,self.position[1]])
+                coordinates.append([self.position[0]+3,self.position[1]])
+            case 1: # +
+                coordinates.append([self.position[0]+1,self.position[1]])
+                coordinates.append([self.position[0],self.position[1]-1])
+                coordinates.append([self.position[0]+1,self.position[1]-1])
+                coordinates.append([self.position[0]+2,self.position[1]-1])
+                coordinates.append([self.position[0]+1,self.position[1]-2])
+            case 2: # _|
+                coordinates.append([self.position[0]+2,self.position[1]])
+                coordinates.append([self.position[0]+2,self.position[1]-1])
+                coordinates.append([self.position[0]+2,self.position[1]-2])
+                coordinates.append([self.position[0]+1,self.position[1]-2])
+                coordinates.append([self.position[0],self.position[1]-2])
+            case 3: # |
+                coordinates.append(self.position)
+                coordinates.append([self.position[0],self.position[1]-1])
+                coordinates.append([self.position[0],self.position[1]-2])
+                coordinates.append([self.position[0],self.position[1]-3])
+            case 4: # #
+                coordinates.append(self.position)
+                coordinates.append([self.position[0]+1,self.position[1]])
+                coordinates.append([self.position[0],self.position[1]-1])
+                coordinates.append([self.position[0]+1,self.position[1]-1])
+
+
+        return coordinates
     def rows(self):
         match self.type:
             case 0: # -
@@ -151,26 +183,34 @@ def create_row(n):
         map.append(['.' for _ in range(7)])
 
 def print_map():
-    for x in map:
+
+    for x in map[::-1]:
         print(x)
 
 with open('inputs/sample.txt') as i: input = [*i.read()]
  
-create_row(top_row)
-#print_map()
+
 r=0
 j=0
-while r < 2022:
+while r < 2:
+    print("Rock Count: %d" % r)
     rock = Rock(r%5)
-    top_row += rock.rows() #set starting - first object
-    rock.position = [2, top_row]
+    top_row += rock.rows() + 3 #set starting - first object
+    print("Top Row: %d" % top_row)
+    create_row(top_row)
+    print("Map Height: %d" % len(map))
+    rock.position = [2, top_row-1]
     floor = False
     while not floor:
+        print("J %d" % j)
         rock.jet_move(input[j%len(input)])
         if not rock.down_move():
-            rock.imprint()
             floor = True
+            for x in rock.imprint():
+                map[x[1]][x[0]] = "#"
+            
         j+=1
     r+=1
-
+print_map()
+print(len(map))
 #top row is +3 units from the top block
